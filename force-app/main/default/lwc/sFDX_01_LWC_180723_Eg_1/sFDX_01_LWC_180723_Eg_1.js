fcf25376af092e0dfd3c2c56c8c9ff4d3ec1a30f
testgithubthr <rane.tushar83@outlook.com>
2023-07-18 15:47:35 +0530

    SFDX First Initialise Commit

---
+++ force-app/main/default/lwc/sFDX_01_LWC_180723_Eg_1/sFDX_01_LWC_180723_Eg_1.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class SFDX_01_LWC_180723_Eg_1 extends LightningElement
+ {
+    callMe()
+    {
+        alert('Hi this is executed from LWC');
+    }
+ }
