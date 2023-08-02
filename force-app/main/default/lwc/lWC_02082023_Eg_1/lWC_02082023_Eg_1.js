11a3e2f19f3dc49273a04919514a4b2c70ab6308
testgithubthr <rane.tushar83@outlook.com>
2023-08-02 15:02:41 +0530

    LWC_First_Example_First_Commit

---
+++ force-app/main/default/lwc/lWC_02082023_Eg_1/lWC_02082023_Eg_1.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class LWC_02082023_Eg_1 extends LightningElement
+{
+    callme()
+    {
+        alert('Hi This is executed from LWC file');
+    }
+
+}
---
+++ force-app/main/default/lwc/sFDX_01_LWC_Accordion_Eg_01/sFDX_01_LWC_Accordion_Eg_01.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class SFDX_01_LWC_Accordion_Eg_01 extends LightningElement {}
